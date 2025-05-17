93eec467e116ad49f489f96261c9426a571170a3
arduclown <maridudkina.30@gmail.com>
2025-05-17 20:39:40 +0700

    Set up PostgreSQL

--- utils/db.go
+++ utils/db.go
@@ -3,14 +3,14 @@ package utils
 import (
 	"database/sql"
 
-	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 var DB *sql.DB
 
 func InitDB() error {
 	var err error
-	DB, err = sql.Open("sqlite3", "./students.db")
+	DB, err = sql.Open("pgx", "host=localhost port=5432 user=myuser password=mypassword dbname=studentapi sslmode=disable")
 	if err != nil {
 		return err
 	}
@@ -26,7 +26,7 @@ func CloseDB() {
 }
 
 func InsertStudent(student Student) error {
-	_, err := DB.Exec("INSERT INTO students (name, age, grade) VALUES (?, ?, ?)", student.Name, student.Age, student.Grade)
+	_, err := DB.Exec("INSERT INTO students (name, age, grade) VALUES ($1, $2, $3)", student.Name, student.Age, student.Grade)
 	return err
 }
 
