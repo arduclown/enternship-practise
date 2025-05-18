4efa1b974eb7a1db403159062f877ac73a193569
arduclown <maridudkina.30@gmail.com>
2025-05-18 10:23:57 +0700

    .

--- goroutine.go
+++ goroutine.go
@@ -2,7 +2,6 @@
 package main
 
 import (
-	"fmt"
 	"sync"
 )
 
@@ -17,31 +16,31 @@ func sumArr(arr []int, start, end int, wg *sync.WaitGroup, res chan int) {
 	res <- sum
 }
 
-func main() {
-	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	nG := 3 // кол-во горутин
+// func main() {
+// 	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+// 	nG := 3 // кол-во горутин
 
-	subarraySize := len(arr) / nG // размер подмассива для передача каждой горутине
+// 	subarraySize := len(arr) / nG // размер подмассива для передача каждой горутине
 
-	res := make(chan int, nG) // буфеизированный канал на nG элементов
-	var wg sync.WaitGroup     //
-	for i := 0; i < nG; i++ {
-		start := i * subarraySize
-		end := start + subarraySize
+// 	res := make(chan int, nG) // буфеизированный канал на nG элементов
+// 	var wg sync.WaitGroup     //
+// 	for i := 0; i < nG; i++ {
+// 		start := i * subarraySize
+// 		end := start + subarraySize
 
-		if i == nG-1 {
-			end = len(arr)
-		}
-		wg.Add(1)
-		go sumArr(arr, start, end, &wg, res)
-	}
+// 		if i == nG-1 {
+// 			end = len(arr)
+// 		}
+// 		wg.Add(1)
+// 		go sumArr(arr, start, end, &wg, res)
+// 	}
 
-	wg.Wait()
-	close(res)
+// 	wg.Wait()
+// 	close(res)
 
-	total := 0
-	for sum := range res {
-		total += sum
-	}
-	fmt.Println("Total sum: ", total)
-}
+// 	total := 0
+// 	for sum := range res {
+// 		total += sum
+// 	}
+// 	fmt.Println("Total sum: ", total)
+// }
